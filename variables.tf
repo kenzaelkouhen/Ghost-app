9f89abec06c7360a61cbd2e182def11a95fdaa2b
Kenza <95191294+kenzaelkouhen@users.noreply.github.com>
2024-10-09 16:53:02 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "aws_access_key" {
-  description = "AWS Access Key ID"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-}
-
 variable "aws_region" {
   description = "The AWS region to deploy resources"
   default     = "us-east-1"
