f8fda9a9173df67ebd02057de44880a05812f453
Kenza <95191294+kenzaelkouhen@users.noreply.github.com>
2024-10-11 10:15:56 +0200

    Update load_balancer.tf

--- load_balancer.tf
+++ load_balancer.tf
@@ -27,7 +27,7 @@ resource "aws_lb_target_group" "my_target_group" {
   name     = "ghost-target-group"
   port     = 2368  # The port Ghost listens on
   protocol = "HTTP"
-  vpc_id   = [data.aws_vpc.default.id]
+  vpc_id   = data.aws_vpc.default.id
 
   health_check {
     healthy_threshold   = 2
